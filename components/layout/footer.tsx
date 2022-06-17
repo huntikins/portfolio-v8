eda36e493c5aa25f6647d9ae05928f2a15800975
Hunter Trammell <huntertrammell@Ceris-Air.lan>
2022-06-16 19:23:44 -0500

    remove supabase from footer

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -5,7 +5,7 @@ export const Footer: FunctionComponent = () => {
   return (
     <footer className="bg-primary text-light p-4 max-w-screen-xl mx-auto text-center">
       <p>&copy; {date} Hunter Trammell</p>
-      <p>Made with ❤️ using Next.js x Supabase </p>
+      <p>Made with ❤️ using Next.js</p>
     </footer>
   );
 };
