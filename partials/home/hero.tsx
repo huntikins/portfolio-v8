c97ee69d23b5cff220dde58cf4a17a8e5b2a4870
Hunter Trammell <39362365+huntikins@users.noreply.github.com>
2024-03-20 08:48:11 -0500

    Update hero.tsx

--- partials/home/hero.tsx
+++ partials/home/hero.tsx
@@ -12,7 +12,7 @@ export const Hero: FunctionComponent = () => {
           <div>
             <Heading level={1}>Hi, I&apos;m Hunter!</Heading>
             <Heading level={2} theme="secondary">
-              Front End Developer, Blogger, Lover of Code.
+              Senior Software Engineer, Blogger, Cetacean Enthusiast, Taco Bell Fanboy, Slinger of Dank Code.
             </Heading>
           </div>
         </div>
@@ -20,23 +20,17 @@ export const Hero: FunctionComponent = () => {
           I&apos;m a Front End Developer hailing from Kansas City, and I&apos;m
           excited to welcome you to my blog! Here, you&apos;ll find a diverse
           range of content, from intriguing short stories to informative
-          coding-related articles and tutorials. I&apos;m also thrilled to
-          announce the recent publication of my book on React, Mastering
-          React.js: A Comprehensive Guide to Building Modern, Scalable Web
-          Applications, which I believe will be a valuable resource for fellow
-          developers. Don&apos;t miss out on this opportunity to elevate your
-          skills - check it out now by clicking here! Happy Coding and enjoy
-          your stay on my blog!
+          coding-related articles and tutorials. Happy Coding and enjoy
+          your stay on my blog!🤘🐋
         </p>
         <div>
           <Button
             label="Check It Out Now!"
             type="link"
-            path="/books/master-react-js-comprehensive-guide-scalable-web-applications"
+            path="mailto:hello@huntertrammell.dev"
           />
         </div>
       </Section>
-      <PromoEBook />
     </>
   );
 };
